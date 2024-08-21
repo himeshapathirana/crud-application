c9f9026adb4346d815cf7a72d54a9db7366793a1
himeshapathirana <94542536+himeshapathirana@users.noreply.github.com>
2024-08-21 16:28:42 +0530

    Update2 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:crud/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,26 +20,28 @@ class MyApp extends StatelessWidget {
       title: "CRUD APPLICATION",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.blueGrey, // Use a predefined MaterialColor here
-          scaffoldBackgroundColor: Colors.black12,
-          primaryColor: Color.fromARGB(255, 11, 27, 37),
-          appBarTheme: AppBarTheme(
-            color: Color.fromARGB(255, 106, 133, 155),
+        primarySwatch: Colors.blueGrey, // Use a predefined MaterialColor here
+        scaffoldBackgroundColor: Colors.black12,
+        primaryColor: const Color.fromARGB(255, 11, 27, 37),
+        appBarTheme: const AppBarTheme(
+          color: Color.fromARGB(255, 106, 133, 155),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 33, 73, 141),
+            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
           ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 33, 73, 141),
-              foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color.fromARGB(255, 0, 0, 0),
             ),
           ),
-          inputDecorationTheme: InputDecorationTheme(
-              border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ))),
-      home: HomeScreen(),
+        ),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
